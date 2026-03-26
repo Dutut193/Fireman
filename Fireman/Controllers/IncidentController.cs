c162dd69f65a39c97c78e504b1d676e399ac8d27
user1 <user1@gmail.com>
2026-03-26 08:52:19 +0200

    Controllers

---
+++ Fireman/Controllers/IncidentController.cs
@@ -0,0 +1,84 @@
+﻿using FIREMAN.Data;
+using FIREMAN.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+public class IncidentController : Controller
+{
+    private readonly FireDbContex _context;
+
+    public IncidentController(FireDbContex context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+        => View(await _context.Incidents.ToListAsync());
+
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null) return NotFound();
+
+        var item = await _context.Incidents.FirstOrDefaultAsync(x => x.IncidentId == id);
+        if (item == null) return NotFound();
+
+        return View(item);
+    }
+
+    public IActionResult Create() => View();
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Incident item)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(item);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(item);
+    }
+
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null) return NotFound();
+
+        var item = await _context.Incidents.FindAsync(id);
+        if (item == null) return NotFound();
+
+        return View(item);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(int id, Incident item)
+    {
+        if (id != item.IncidentId) return NotFound();
+
+        if (ModelState.IsValid)
+        {
+            _context.Update(item);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(item);
+    }
+
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null) return NotFound();
+
+        var item = await _context.Incidents.FirstOrDefaultAsync(x => x.IncidentId == id);
+        if (item == null) return NotFound();
+
+        return View(item);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var item = await _context.Incidents.FindAsync(id);
+        _context.Incidents.Remove(item);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+}
---
+++ Fireman/Controllers/TeamsController.cs
@@ -0,0 +1,83 @@
+﻿using FIREMAN.Data;
+using FIREMAN.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+public class TeamsController : Controller
+{
+    private readonly FireDbContex _context;
+
+    public TeamsController(FireDbContex context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+        => View(await _context.Teams.ToListAsync());
+
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null) return NotFound();
+
+        var item = await _context.Teams.FirstOrDefaultAsync(x => x.TeamId == id);
+        if (item == null) return NotFound();
+
+        return View(item);
+    }
+
+    public IActionResult Create() => View();
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Teams item)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(item);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(item);
+    }
+
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null) return NotFound();
+
+        var item = await _context.Teams.FindAsync(id);
+        if (item == null) return NotFound();
+        return View(item);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(int id, Teams item)
+    {
+        if (id != item.TeamId) return NotFound();
+
+        if (ModelState.IsValid)
+        {
+            _context.Update(item);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(item);
+    }
+
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null) return NotFound();
+
+        var item = await _context.Teams.FirstOrDefaultAsync(x => x.TeamId == id);
+        if (item == null) return NotFound();
+
+        return View(item);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var item = await _context.Teams.FindAsync(id);
+        _context.Teams.Remove(item);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+}
